40173ef8bb4931efc7a9f9b7aebe2f14ea2c0a00
karlos g liberal <karlosgliberal@gmail.com>
2017-12-18 12:29:22 +0100

    un apaño para cambiar las barbas

--- landing/src/sketch/sketch_cam.js
+++ landing/src/sketch/sketch_cam.js
@@ -21,6 +21,7 @@ var _movingSize = 0.7;
 var translateBarba = 200
 var capture;
 var videoInput;
+var visible = true;
 
 
 function centerCanvas() {
@@ -34,6 +35,7 @@ function randomDos(){
 }
 
 function setup() {
+  barbasId = [randomDos()];
   for (var i = 0; i < barbasId.length; i++) {
     getData(barbasId[i]);
   }
@@ -58,63 +60,66 @@ function setup() {
 }
 
 function draw() {
-  if(typeof(barbas) != 'undefined'){
+  if(visible){
     clear();
+  }
+  if(typeof(barbas) != 'undefined'){
     locura(barbas);
   }
 }
 
-function locura(barbas){
-  clear();
-  image(videoInput,0,0,600,400)
-  var positions = ctracker.getCurrentPosition();
-  if(positions.length > 0) {
-    var p1 = createVector(positions[7][0], positions[7][1] );
-    var p2 = createVector(positions[33][0], positions[33][1] );
+function locura(barbas, visible){
+    clear();
+    image(videoInput,0,0,600,400)
+    var positions = ctracker.getCurrentPosition();
+    if(positions.length > 0) {
+      var p1 = createVector(positions[7][0], positions[7][1] );
+      var p2 = createVector(positions[33][0], positions[33][1] );
 
-    var angleRad = Math.atan2(p2.y - p1.y, p2.x - p1.x);
+      var angleRad = Math.atan2(p2.y - p1.y, p2.x - p1.x);
 
-    translate(positions[0][0]-20, positions[70][1]+10);
-    rotate(angleRad + PI/2);
-    var mSize = p1.dist(p2);
-    strokeWeight(3);
-    stroke(255,255 ,255);
+      translate(positions[0][0]-20, positions[70][1]+10);
+      rotate(angleRad + PI/2);
+      var mSize = p1.dist(p2);
+      strokeWeight(3);
+      stroke(255,255 ,255);
 
-    scale(0.6* (mSize*0.01), 0.6* (mSize*0.01));
-    var _movingSize = 0.8;
-    var drawDataMoving = barbas;
-    var lastSeenKey;
+      scale(0.6* (mSize*0.01), 0.6* (mSize*0.01));
+      var _movingSize = 0.8;
+      var drawDataMoving = barbas;
+      var lastSeenKey;
 
-    for (var b = 0; b < drawDataMoving.length; b++) {
-      for (let i = 0; i < drawDataMoving[b].drawing.length; i++) {
-        for (let j = 0; j < drawDataMoving[b].drawing[i].length; j += 2) {
-          for (let k = 0; k < drawDataMoving[b].drawing[i][j].length - 1; k++) {
-          //if(k < drawDataMoving[b].drawing[i][j + 0].length - 1){
-          let _x1 = drawDataMoving[b].drawing[i][j + 0][k] * _movingSize;
-          let _y1 = drawDataMoving[b].drawing[i][j + 1][k] * _movingSize;
-          let _x2 = drawDataMoving[b].drawing[i][j + 0][k + 1] * _movingSize;
-          let _y2 = drawDataMoving[b].drawing[i][j + 1][k + 1] * _movingSize;
-          line(_x1, _y1, _x2, _y2);
+      for (var b = 0; b < drawDataMoving.length; b++) {
+        for (let i = 0; i < drawDataMoving[b].drawing.length; i++) {
+          for (let j = 0; j < drawDataMoving[b].drawing[i].length; j += 2) {
+            for (let k = 0; k < drawDataMoving[b].drawing[i][j].length - 1; k++) {
+              //if(k < drawDataMoving[b].drawing[i][j + 0].length - 1){
+              let _x1 = drawDataMoving[b].drawing[i][j + 0][k] * _movingSize;
+              let _y1 = drawDataMoving[b].drawing[i][j + 1][k] * _movingSize;
+              let _x2 = drawDataMoving[b].drawing[i][j + 0][k + 1] * _movingSize;
+              let _y2 = drawDataMoving[b].drawing[i][j + 1][k + 1] * _movingSize;
+              line(_x1, _y1, _x2, _y2);
+            }
           }
         }
       }
     }
-
-  }
 }
 
 function keyPressed() {
   if (keyCode === LEFT_ARROW) {
-    clear();
+    location.reload();
   } else if (keyCode === RIGHT_ARROW) {
-    clear();
-    barbasId = [randomDos()];
-    getData(barbasId[0]);
+    location.reload();
   } else if (keyCode === ENTER) {
-    save("movida.jpg");
+    save("felizCiberbarba2018.jpg");
   }
 }
 
+function negro(){
+  background(0);
+  clear();
+}
 
 function getData(id) {
   firebase.database().ref('/barbas/'+id).once('value').then(function(snapshot) {
@@ -124,7 +129,6 @@ function getData(id) {
     barbas.push(snapshot.val());
     //}
   }).then(function(){
-    clear();
   });
 }
 
