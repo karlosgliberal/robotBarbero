196b26f9bbd52d2e37c9754bfacd137e70f7e085
karlos g liberal <karlosgliberal@gmail.com>
2017-12-21 12:41:33 +0100

    barba limpia

--- web/src/js/sketch_barbas.js
+++ web/src/js/sketch_barbas.js
@@ -11,7 +11,7 @@ firebase.initializeApp(config);
 var database = firebase.database();
 var rnd = 1;
 var barbas = [];
-var barbasId = [686, randomDos(), randomDos(), randomDos(), randomDos()];
+var barbasId = [686, 214, 1, randomDos(), randomDos()];
 var barbasIteradores = [0, 0, 0, 0, 0];
 var cnv;
 var _movingSize = 0.6;
@@ -52,6 +52,7 @@ function locura(barbas){
   translate(10, 10);
 
   for (var b = 0; b < drawDataMoving.length; b++) {
+    console.log(drawDataMoving);
     for (let i = 0; i < drawDataMoving[b].drawing.length; i++) {
       if(barbasIteradores[b] < drawDataMoving[b].drawing[i][0].length - 1){
         let _x1 = drawDataMoving[b].drawing[i][0][barbasIteradores[b]] * _movingSize;
@@ -69,18 +70,9 @@ function locura(barbas){
 
 function keyPressed() {
   if (keyCode === LEFT_ARROW) {
-    console.log("left");
-    var dato = '{"word":"beard","countrycode":"IN","timestamp":"2017-03-18 17:27:42.74233 UTC","recognized":true,"key_id":"4919746145288192","drawing":[[[51,65,77,81,87,93,100,103,112,121,125,129,142,164,174,173,       163,154,135,93,53,32,21,11,6,0,1,18],[149,215,254,254,236,251,235,242,218,244,233,241,142,131,116,89,62,45,29,10,0,1,4,12,20,50,70,99]]]}';
-    var datoObj = JSON.parse(dato);
-    console.log(datoObj);
-    console.log(JSON.parse(datoObj));
     barbasId.shift();
-    barbasId.push(dato)
-    console.log(barbasId);
-    for (var i = 0; i < barbasId.length; i++) {
-      getData(barbasId[i]);
-    }
-
+    barbasIteradores.shift();
+    getData(1);
     clear();
   } else if (keyCode === DOWN_ARROW) {
     getData();
@@ -100,6 +92,8 @@ function getData(id) {  // preload() runs once
   }).then(function(){
   });
 }
+
+
 
 function centerCanvas() {
   var x = (windowWidth - width) / 2;
